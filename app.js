490db62a0ddc77d6229081e1e853eec0760f441f
omarsagoo <om.sagoo@gmail.com>
2020-03-31 10:48:14 -0700

    [add] new routes and templates

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 // Require Libraries
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 // App Setup
 const app = express();
 
 // Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.get("/", (req,res)=>{
-    res.send("Hello World!")
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
 })
 
-// Start Server
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings', { name });
+  })
 
+// Start Server
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
